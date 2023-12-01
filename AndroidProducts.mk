ac82e710625fe851f36fc6edbac1c2ff25c7ca18
ppanzenboeck <philipp.panzenboeck1@gmail.com>
2023-12-01 15:41:42 +0100

    adapt to derpfest

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_enchilada.mk
+    $(LOCAL_DIR)/derp_enchilada.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_enchilada-user \
-    lineage_enchilada-userdebug \
-    lineage_enchilada-eng
+    derp_enchilada-user \
+    derp_enchilada-userdebug \
+    derp_enchilada-eng
